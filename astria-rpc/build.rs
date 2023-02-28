a4ec66a1ee427333efab1c548c4fb8d9b589474b
Jesse Snyder <jessetsnyder@gmail.com>
2023-02-28 13:32:12 -0700

    use tonic_build builder api

--- astria-rpc/build.rs
+++ astria-rpc/build.rs
@@ -1,11 +1,12 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=proto/");
 
-    let protos = vec!["proto/execution.proto"];
-
-    for proto in protos {
-        tonic_build::compile_protos(proto)?;
-    }
+    tonic_build::configure()
+        .build_server(false)
+        .compile(
+            &["proto/execution.proto"],
+            &["proto"],
+        )?;
 
     Ok(())
 }
