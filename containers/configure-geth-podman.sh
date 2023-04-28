549b2de3f4eda4bb39cb7ee21e357ceb187a3b9b
Jesse Snyder <jessetsnyder@gmail.com>
2023-04-28 12:33:24 -0600

    fix geth env. fix geth volume. fix genesis.json location.

--- containers/configure-geth-podman.sh
+++ containers/configure-geth-podman.sh
@@ -6,10 +6,11 @@ DEFAULT_ACCOUNT_ID="0xb0E31D878F49Ec0403A25944d6B1aE1bf05D17E1"
 # use default account id if evm_address envar is not set
 ACCOUNT_ID=${evm_address:-$DEFAULT_ACCOUNT_ID}
 
-mv genesis.json genesis.bak.json
+echo "Modifying genesis.json to allocate funds to $ACCOUNT_ID"
 
-# TODO - use dasel instead of jq?
+mv /genesis.json $home_dir/genesis.bak.json
+
 # use jq to replace alloc value in genesis.json with ACCOUNT_ID envar
 jq --arg accountId "$ACCOUNT_ID" \
   '.alloc |= with_entries( if .key | startswith("0x") then .key = $accountId else . end )' \
-  genesis.bak.json > genesis.json
+  $home_dir/genesis.bak.json > $home_dir/genesis.json
