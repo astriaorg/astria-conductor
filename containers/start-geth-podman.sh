6eedae20181e6ffaa8563274347a123a95dd8e2f
Jesse Snyder <jessetsnyder@gmail.com>
2023-05-01 23:40:41 -0600

    paramaterize ports. fix typos

--- containers/start-geth-podman.sh
+++ containers/start-geth-podman.sh
@@ -2,8 +2,7 @@
 
 set -o errexit -o nounset
 
-# TODO - parameterize some of these ports
-geth --datadir $home_dir/.astriageth/ --http --http.addr "0.0.0.0" --http.port=8545 \
-  --ws --ws.addr "0.0.0.0" --ws.port=8545 --networkid=1337 --http.corsdomain='*' --ws.origins='*' \
-  --grpc --grpc.addr "0.0.0.0" --grpc.port 50051 \
-  --metro.addr "0.0.0.0" --metro.port 9100
+geth --datadir $home_dir/.astriageth/ --http --http.addr "0.0.0.0" --http.port=$executor_host_http_port \
+  --ws --ws.addr "0.0.0.0" --ws.port=$executor_host_http_port --networkid=1337 --http.corsdomain='*' --ws.origins='*' \
+  --grpc --grpc.addr "0.0.0.0" --grpc.port $executor_host_grpc_port \
+  --metro.addr "0.0.0.0" --metro.port $sequencer_host_grpc_port
